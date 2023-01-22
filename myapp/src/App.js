d6865b1ea5c5841470755dbc45e3a96a5d106d5a
seb4594 <65371156+seb4594@users.noreply.github.com>
2023-01-22 17:50:12 -0600

    Added update function

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -2,9 +2,22 @@ import './App.css';
 import { DataStore } from '@aws-amplify/datastore';
 import { Todo } from './models';
 
-function App()  {
-  async function addTodo() {
+function App() {
+  
+  async function update() {
+    // Use DataStore obj to query, passing the data type and id
+    const orginal = await DataStore.query(Todo, "c2c3e450-965f-4ddd-ad1f-42dda6b6ade9")
 
+    /* Models in DataStore are immutable. To update a record you must use the copyOf function
+    to apply updates to the item’s fields rather than mutating the instance directly */
+    await DataStore.save(Todo.copyOf(orginal, item => {
+        // Update the values on {item} variable to update DataStore entry
+      item.name = `title: ${Date.now()}`
+    }));
+    
+  }
+
+  async function addTodo() {
     await DataStore.save(
     new Todo({
       "name": "Lorem ipsum dolor sit amet",
@@ -26,6 +39,7 @@ function App()  {
     <div className="App">
       <button onClick={addTodo}> Add </button>
       <button onClick={logData}> Log </button>
+      <button onClick={update}> Update </button>
     </div>
   );
 }
